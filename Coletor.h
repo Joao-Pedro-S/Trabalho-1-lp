c0ebc37f4d89b981fd900e7f22a80aa6d136e778
JPSilvaSantos <19837293-JPSilvaSantos@users.noreply.replit.com>
2023-12-14 14:31:02 +0000

    Initial commit

---
+++ Coletor.h
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*como char possui tamanho de 1 byte,
+isto é usado para navegar entre o endereço usado para o contador
+e o dado guardado na variável.
+Talvez não seja necessário, mas evitou um bug que ocorria com void*/
+
+void *malloc2(int tamanho) {
+  char *aux = (char *)malloc(sizeof(int) + tamanho);
+  int *count = (int *)aux;
+  *count = 1;
+  void *ret; // retorno
+  ret = aux + sizeof(int);
+  return ret;
+}
+///* pra teste
+int quant(void *x) {
+  void *end = (char *)x - sizeof(int);
+  int *cont = (int *)end;
+  return *cont;
+}
+//*/
+void atrib2(void **x, void *y) {
+  if (y == NULL) {
+    int *aux = (int *)*x - 1;
+    *aux = *aux - 1;
+    *x = NULL;
+    if (*aux == 0) {
+      free(*x);
+      free(aux);
+    }
+    return;
+  }
+  int *aux = (int *)*x - 1;
+  void *aux2 = *x;
+  int *aux3 = (int *)y - 1;
+  *aux = *aux - 1;
+  *x = y;
+  *aux3 = *aux3 + 1;
+  if (*aux == 0) {
+    free(aux2);
+    free(aux);
+  }
+}
---
+++ main.c
@@ -0,0 +1,26 @@
+#include "Coletor.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int *v = malloc2(sizeof(int));
+  *v = 10;
+  printf("%d %d\n", *v, quant(v));
+  int *w = malloc2(sizeof(int));
+  *w = 20;
+  printf("%d %d\n", *w, quant(w));
+  atrib2((void**)&v, (void *)w);
+  printf("%d %d\n", *w, quant(w));
+  printf("%d %d\n", *v, quant(v));
+  atrib2((void **)&w, NULL);
+  printf("%d %d\n", *v, quant(v));
+  char *s = (char *)malloc2(sizeof(char) * 4);
+  *s = 'B';
+  printf("%c %d", *s, quant(s));
+  atrib2((void **)&s, NULL);
+  /* //teste de NULL, funciona.
+  if (s == NULL)
+    printf("a");
+  */
+  return 0;
+}
